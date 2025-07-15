eb36c4f7b119e4f15427163ab1305ca41d30b88c
Luisa Coakley <48064300+lacoak21@users.noreply.github.com>
2025-07-15 13:02:26 -0600

    ULTRA L1A ExTOF high time  (#1937)
    * process ultra l1a extof high time data

--- imap_processing/ultra/l1a/ultra_l1a.py
+++ imap_processing/ultra/l1a/ultra_l1a.py
@@ -25,6 +25,7 @@ from imap_processing.ultra.l0.ultra_utils import (
     ULTRA_ENERGY_SPECTRA,
     ULTRA_EVENTS,
     ULTRA_EXTOF_HIGH_ANGULAR,
+    ULTRA_EXTOF_HIGH_TIME,
     ULTRA_HK,
     ULTRA_MACROS_CHECKSUM,
     ULTRA_PHXTOF_HIGH_ANGULAR,
@@ -97,6 +98,7 @@ def ultra_l1a(  # noqa: PLR0912
             ULTRA_PHXTOF_HIGH_ENERGY,
             ULTRA_PHXTOF_HIGH_TIME,
             ULTRA_EXTOF_HIGH_ANGULAR,
+            ULTRA_EXTOF_HIGH_TIME,
         ]
         for apid in group.apid
     }
