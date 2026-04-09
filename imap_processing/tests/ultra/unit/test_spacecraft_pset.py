7d48939299079ce788c972fce77bbbdeb3d3e7b7
Luisa Coakley <48064300+lacoak21@users.noreply.github.com>
2026-04-09 09:54:14 -0600

    ULTRA l1b upstream ion culling algorithm  (#2931)
    * code and validation tests for upstream ion cull
    
    * add new arrays to mock goodtimes datasets

--- imap_processing/tests/ultra/unit/test_spacecraft_pset.py
+++ imap_processing/tests/ultra/unit/test_spacecraft_pset.py
@@ -179,6 +179,8 @@ def test_calculate_spacecraft_pset_with_cdf(
         de_dict["quality_scattering"] = np.zeros(len(sc_dps_velocity), dtype=np.uint16)
         de_dict["quality_outliers"] = np.zeros(len(sc_dps_velocity), dtype=np.uint16)
         de_dict["ebin"] = np.ones(len(sc_dps_velocity), dtype=np.uint8)
+        de_dict["theta"] = np.zeros(len(df_subset), dtype=np.float32)
+        de_dict["phi"] = np.zeros(len(df_subset), dtype=np.float32)
         de_dict["event_times"] = 817561854.185627 + (
             df_subset["tdb"].values - df_subset["tdb"].values[0]
         )
